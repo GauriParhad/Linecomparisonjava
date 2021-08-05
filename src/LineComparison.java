fde1b91f2c6722bdf9bf164154af532069f9d6e3
Gaurip10 <thegauriparhad@gmail.com>
2021-08-05 18:08:08 +0530

    calculation

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -2,6 +2,12 @@ public class LineComparison {
     public static void main(String[] args)
     {
         System.out.println("WELCOME TO LINE COMPARISON COMPUTATION PROGRAM");
+        int x1 = 2;
+        int x2 = 4;
+        int y1 = 6;
+        int y2 = 9;
+        double lengthofline1 = Math.sqrt((x2 -x1)*(x2-x1) + (y2-y1)*(y2-y1));
+        System.out.println(lengthofline1);
 
     }
 
