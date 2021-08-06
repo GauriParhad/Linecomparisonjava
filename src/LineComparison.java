abebc5f2034bd76da3c182a5efc6e5fd4f1a0b17
Gaurip10 <thegauriparhad@gmail.com>
2021-08-06 09:21:46 +0530

    Lines are equal or not

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -6,7 +6,7 @@ public class LineComparison {
         int y2 = 9;
 
         double lengthofline1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-        System.out.println("Length of line1 is :" + lengthofline1);
+        System.out.println("Length of line1 is :"+lengthofline1);
 
 
         int x3 = 4;
@@ -14,9 +14,30 @@ public class LineComparison {
         int y3 = 8;
         int y4 = 11;
 
-            double lengthofline2 = Math.sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3));
-            System.out.println("Length of line2 is :" + lengthofline2);
+        double lengthofline2 = Math.sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3));
+        System.out.println("Length of line2 is :"+lengthofline2);
+
+        {
+            int a1, a2;
+
+            a1 = (y2 - y1) / (x2 - x1);
+            a2 = (y4 - y3) / (x4 - x3);
+
+            if (a1 == a2)
+                System.out.println("line1 and line2 are equal");
+            else
+                System.out.println("line1 and line2 are not equal");
+        }
+
+        {
+
+            if (lengthofline1 == lengthofline2)
+                System.out.println("It's Equal");
+            else
+                System.out.println("It's not Equal");
+        }
 
     }
-
 }
+
+
