0ba13577e67a23b67806ed912259c4244c361ecc
Gaurip10 <thegauriparhad@gmail.com>
2021-08-06 08:41:16 +0530

    Length of lines

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -1,15 +1,22 @@
 public class LineComparison {
-    public static void main(String[] args)
-    {
-        System.out.println("WELCOME TO LINE COMPARISON COMPUTATION PROGRAM");
+    public static void main(String[] args) {
         int x1 = 2;
         int x2 = 4;
         int y1 = 6;
         int y2 = 9;
-        double lengthofline1 = Math.sqrt((x2 -x1)*(x2-x1) + (y2-y1)*(y2-y1));
-        System.out.println(lengthofline1);
+
+        double lengthofline1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        System.out.println("Length of line1 is :" + lengthofline1);
+
+
+        int x3 = 4;
+        int x4 = 7;
+        int y3 = 8;
+        int y4 = 11;
+
+            double lengthofline2 = Math.sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3));
+            System.out.println("Length of line2 is :" + lengthofline2);
 
     }
-
 
 }
